91c0a8fc4d1680207a6a814c8072b96c4108b0e4
Akash Patel <47402620+Skyrex98@users.noreply.github.com>
2021-02-20 11:55:41 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://AkashPatel98:<password>@cluster0.idgaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      "mongodb+srv://AkashPatel98:Sky4me98@cluster0.idgaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
         useCreateIndex: true,
